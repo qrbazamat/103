d603c55f43d4ec766a2f160ece8e4702b24d8d2f
Azamat <qrbazamat@gmail.com>
2024-12-28 09:41:32 +0500

    - Documents all page ending design; - Information Service: PhotoGallery 50%

---
+++ dist/assets/index-QlU0leN0.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-ebbfaJ8P.js";function a({className:r}){return e.jsx("hr",{className:r,style:{borderImage:"linear-gradient(90deg, transparent, #a5a7eb, transparent) 1"}})}export{a as L};
