0df66e3465eab1461eb37f22a2296aa41529cceb
AzamatCoder <60395192+qrbazamat@users.noreply.github.com>
2025-01-06 11:59:47 +0500

    Delete dist/assets directory

--- dist/assets/image_6-dOlECGv0.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/image_1-xhMzs-pl.jpg",a="/assets/image_2-q7C5Z3TB.jpg",g="/assets/image_3-qK3fS5f8.jpg",e="/assets/image_4-WMMho3Fm.jpg",m="/assets/image_5-22WmFknq.jpg",i="/assets/image_6-1B3KuzfI.jpg";export{a,g as b,e as c,m as d,i as e,s as i};
--- dist/assets/video1-mHkqe5qq.js
+++
@@ -1,1 +0,0 @@
-const e="/assets/video1-J6XQUGrJ.mp4";export{e as B};
